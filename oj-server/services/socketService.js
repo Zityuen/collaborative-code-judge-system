1720ea711dc44ea78a2c2f8610f62171de049684
zityuen <zityuen@gmail.com>
2018-04-04 22:38:33 -0700

    add cursor

--- oj-server/services/socketService.js
+++ oj-server/services/socketService.js
@@ -16,17 +16,32 @@ module.exports = function(io) {
 
         socket.on('change', delta => {
             console.log("change " + socketIdSessionId[socket.id] + " " + delta);
+            forwardEvents(socket.id, 'change', delta);
+        });
+
+        socket.on('cursorMove', cursor => {
+            console.log("cursor " + socketIdSessionId[socket.id] + " " + cursor);
             let sessionId = socketIdSessionId[socket.id];
+            cursor = JSON.parse(cursor);
+            cursor['socketId'] = socket.id;
+
+            forwardEvents(socket.id, 'cursorMove', JSON.stringify(cursor));
+        });
+
+        function forwardEvents(socketId, eventName, dataString) {
+            let sessionId = socketIdSessionId[socketId];
+
             if (sessionId in collaborations) {
                 let participants = collaborations[sessionId]['participants'];
                 for (let i = 0; i < participants.length; i++) {
-                    if(socket.id != participants[i]) {
-                        io.to(participants[i]).emit("change", delta);
+                    if(socketId != participants[i]) {
+                        io.to(participants[i]).emit(eventName, dataString);
                     }
                 }
             } else {
                 console.log("WARNING: could not tie socketId to any collaborations");
             }
-        })
-    })
+        }
+    });
+
 }
